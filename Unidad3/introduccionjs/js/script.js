09970a18129ccecdaf2bd91d61dd39f857910209
DarkZeyan <esonic0234@hotmail.com>
2023-10-04 10:11:58 -0600

    Cambios varios

---
+++ Unidad3/introduccionjs/js/script.js
@@ -0,0 +1,35 @@
+let num =  25.0;
+
+let cadena  = 'Esto es una cadena';
+
+let cad = "esto es una cadena";
+
+console.log('El valor de num es ', num );
+
+// La siguiente es una plantilla literal.
+
+let cadenaPlantilla = `Esta es una plantilla literal 
+ y puede usar mas de una linea`;
+
+console.log(cadenaPlantilla);
+
+num = 'Num ahora es una cadena';
+
+console.log(num);
+
+let num3 =  25;
+
+console.log('El valor de num3 es numerico: ', num3);
+
+const suma = (x1,x2) => x1+x2;
+
+let x1,x2,res;
+x1=5;
+x2=15;
+
+res = suma(x1,x2);
+
+console.log('La suma de x1 y x2 es ', res);
+
+
+
